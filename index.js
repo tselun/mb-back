1b853f43a370a6e707eed654b9c4e6a58770639a
tselun <tsc8@sfu.ca>
2021-02-05 22:39:40 -0800

    m

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const { Client } = require('pg');
+const express = require('express');
+const app = express();
 
+const port = process.env.PORT;
+app.listen(port, () => { console.log(`port: ${port}`) });
+
+const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
-
 client.connect();
 
-client.query('SELECT * FROM mb;', (err, res) => {
-  console.log(JSON.stringify(res));
-
-  client.end();
+app.get('/', () => {
+    client.query('SELECT * FROM mb;', (err, res) => {
+        const data = JSON.stringify(res);
+        res.send(data);
+        
+        // client.end();
+    });
 });
+
+
+
+
